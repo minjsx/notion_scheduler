c0aa17d6bf0ecde1fb54318f86380b322976c20d
Minjsx <tyo2002@gmail.com>
2023-05-29 00:08:28 +0900

    Change to Structure by function

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,33 @@
 import { Client } from "@notionhq/client";
-import { PageObjectResponse, QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
+import { PageObjectResponse, QueryDatabaseResponse, UpdatePageResponse } from "@notionhq/client/build/src/api-endpoints";
 import dotenv from "dotenv";
 import DBTABLES from './tables';
 
 dotenv.config();
 
 async function main() {
-  const notion = new Client({
+   const notion = new Client({
     auth: process.env.NOTION_TOKEN,
   });
+  resetCheckbox(notion);
+}
 
+async function getDatabyDbId(notion : Client, database_id : string) : Promise<QueryDatabaseResponse> {
+  return notion.databases.query({database_id})
+}
+
+async function resetCheckbox(notion : Client) : Promise<Promise<UpdatePageResponse>[]>{
   const api_master = await notion.search({
     filter:{
       property: "object",
       value : "database",
     }
-  })
-  
+  })  
   const databaseID = api_master.results[DBTABLES.RAIDSHEET].id;
   const response = await getDatabyDbId(notion,databaseID);
-  const result = response.results as Array<PageObjectResponse>;
-
-  const updateResponse = await result.map((val, idx) => notion.pages.update({
-                                  page_id : val.id,
+  const result  = response.results as Array<PageObjectResponse>;
+  const updateResponse = await result.map((page) => notion.pages.update({
+                                  page_id : page.id,
                                   properties : {
                                   '완료' : {
                                     type : "checkbox",
@@ -30,12 +35,7 @@ async function main() {
                                   }}
                                 })
                               )
-              
-  // const res = await Promise.all(updateResponse);
-}
-
-async function getDatabyDbId(notion : Client, database_id : string) : Promise<QueryDatabaseResponse> {
-  return notion.databases.query({database_id})
+  return updateResponse;
 }
 
 
